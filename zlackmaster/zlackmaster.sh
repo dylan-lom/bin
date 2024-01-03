33ef1eecf40004ca2fb8e9df387abce41ebd128c
djl <djl@dylanlom.com>
2024-01-03 14:18:59 +1100

    Add cowsay and happy monday features to zlackmaster

--- zlackmaster/zlackmaster.sh
+++ zlackmaster/zlackmaster.sh
@@ -6,9 +6,10 @@
 # 	* curl
 # 	* gnu/obsd date
 # 	* cc (any tm)
+# 	* imagemagick
+# 	* cowsay
 #
 # get channels with: get "/conversations.list" | jq '.channels[] | .id, .name'
-
 progname="$0"
 
 token="$1"
@@ -20,6 +21,20 @@ if [ -z "$token" -o -z "$from" -o -z "$to" ]; then
 	exit 1
 fi
 
+happymonday() {
+	curl -so cat.jpg $(curl -s https://api.thecatapi.com/v1/images/search | jq '.[0].url' | tr -d '"')
+	convert cat.jpg -scale 400x400\! cat.jpg
+	name="$(date +%F).jpg"
+	composite -gravity center happymonday.png cat.jpg "$name"
+
+	mv "$name" "/usr/local/www/p.dlom.cc/monday/$name"
+	echo "http://p.dlom.cc/monday/$name"
+}
+
+ismonday() {
+	test "$(date +%u)" -eq 1
+}
+
 # Compat for passing in a timestamp to date(1). Tested with GNU and OpenBSD date
 cdate() {
 	ts="$1"
@@ -30,6 +45,12 @@ cdate() {
 
 unquote() {
 	sed 's/^"\(.*\)"$/\1/'
+}
+
+jsonify() {
+	sed 's/\([\]\)/\\\1/g' \
+		| sed 's/$/\\n/' \
+		| tr -d '\n'
 }
 
 get() {
@@ -61,10 +82,10 @@ dupeMessage() {
 	text="$(echo "$msg" | jq '.text' | unquote)"
 	attachments="$(echo "$msg" | jq '.files[].url_private' 2>/dev/null | unquote)"
 
-	text="$(echo "($recv) $sender: $text $attachments")"
+	text="($recv) $sender: $text $attachments"
 	test -z "$text" && text="slack lmao"
 
-	echo "{\"channel\": \"$to\", \"text\": \"$text\" }"
+	data="{\"channel\": \"$to\", \"text\": \"\`\`\`\\n$(printf "%s" "$text" | cowsay | jsonify)\\n\`\`\`\" }"
 }
 
 post "/conversations.join" --data "{ \"channel\": \"$from\" }" > /dev/null
@@ -76,7 +97,15 @@ echo "Reviving messages before: $latest"
 
 for ts in $(get "/conversations.history?channel=$from&latest=$latest" | jq '.messages[].ts' | unquote | sort); do
 	echo "INFO: Reviving message from $ts"
-	data="$(dupeMessage $ts)"
+	dupeMessage $ts
+	printf "%s" "$data"
+
 	post "/chat.postMessage" --data "$data"
 	sleep 1 # https://api.slack.com/methods/chat.postMessage#rate_limiting
 done
+
+if ismonday; then
+	text="$(happymonday)"
+	post "/chat.postMessage" --data "{\"channel\": \"$to\", \"text\": \"$text\"}"
+fi
+
